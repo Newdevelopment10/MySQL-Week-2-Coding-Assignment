54c72da23482cab9de86dd8ba4f8507fbe8db03f
Mark AS <93153073+Newdevelopment10@users.noreply.github.com>
2022-01-08 15:06:37 -0800

    Add files via upload

---
+++ Week2sqlcodassign.sql
@@ -0,0 +1,11 @@
+SELECT COUNT(*) AS 'Titles Born After 1965-01-01', t.title FROM employees e 
+INNER JOIN titles t USING (emp_no) WHERE birth_date > '1965-01-01' GROUP BY t.title LIMIT 20;
+
+SELECT AVG(s.salary) AS 'Average Salary', t.title FROM salaries s
+INNER JOIN titles t USING (emp_no) GROUP BY t.title;
+
+SELECT SUM(s.salary) AS 'Total Spent', d.dept_name FROM salaries s
+INNER JOIN dept_emp de USING (emp_no)
+INNER JOIN departments d USING (dept_no) WHERE year(s.from_date) >= 1990 AND year(s.to_date) <= 1992
+AND d.dept_name = 'Marketing' 
+GROUP BY dept_name;
